400af2bd43bb33448d8006763a69cb2059ee0859
Draked8 <drakedurrett8@gmail.com>
2020-10-07 13:25:50 -0500

    hosting

--- server/index.js
+++ server/index.js
@@ -72,7 +72,8 @@ app.get('/sign-s3', (req, res) => {
 });
 
 // Hosting Endpoint 
-app.use(express.static(__dirname + '/../build'));
+app.use(express.static(`${__dirname}/../build`));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'))
 });
